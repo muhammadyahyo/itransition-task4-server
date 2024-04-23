5dea81745f8ac7d2283cfb9398006537e4f2a82b
muhammadyahyo <74825893+muhammadyahyo@users.noreply.github.com>
2024-04-23 23:34:19 +0500

    first commit

--- src/middlewares/corsMiddleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const corsResolver = (request, response, next) => {
-    response.header('Access-Control-Allow-Origin', '*');
-    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    response.setHeader('Access-Control-Allow-Headers', '*');
-    next();
-};
-exports.default = corsResolver;
